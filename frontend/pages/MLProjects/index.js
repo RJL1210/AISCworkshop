cbf28e9e22c892de1b9919dec46d714c9287d3fd
RJL1210 <144273649+RJL1210@users.noreply.github.com>
2024-02-03 15:58:45 -0800

    change some text

--- frontend/pages/MLProjects/index.js
+++ frontend/pages/MLProjects/index.js
@@ -1,10 +1,11 @@
 import styles from '../../styles/MLProjects.module.scss';
 import Image from 'next/image';
 
+
 export default function Home() {
     return (
         <div className={styles.container}>
-            <h1 className={styles.container_title}>HackDavis x AISC <br />Machine Learning Projects</h1>
+            <h1 className={styles.container_title} style={{color: "#558DCA"}}>Ryan Li <br />Machine Learning Projects</h1>
             <div className={styles.container_inside}>
                 <div className={styles.container_button}>
                     <a href="/MLProjects/breastCancer" style={{textDecoration: 'none', fontWeight: 'bold'}}>MLProjects</a>
