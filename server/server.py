e855c5a4bb9ee47d15e10671e0969dd697130b32
RJL1210 <144273649+RJL1210@users.noreply.github.com>
2024-02-03 15:17:57 -0800

    server.py and frontend indexMsg/index

--- server/server.py
+++ server/server.py
@@ -1,7 +1,24 @@
 from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
-# from algorithm import run_algorithm
+#from algorithm import run_algorithm
 from main import run_algorithm
 from eda import analysis
 from flask_restful import Api, Resource
+
+app = Flask(__name__)
+
+CORS(app) #allow frontend and backend to work together
+api = Api(app)
+
+@app.route("/", methods=['GET']) #route to homePage
+def get_home():
+    #return info as JSON
+    return jsonify({ 
+        'message': "Jsonifed",
+        'people': ["Ryan", "Amanda", "Ken"]
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8080)
+
 
